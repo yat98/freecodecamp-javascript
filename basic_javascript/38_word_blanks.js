bd3a382ede83bfc6be0760af2fee754cfc56c943
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-13 20:30:47 +0800

    Basic javascript: 38. word blanks

---
+++ basic_javascript/38_word_blanks.js
@@ -0,0 +1,14 @@
+const myNoun = 'dog';
+const myAdjective = 'big';
+const myVerb = 'ran';
+const myAdverb = 'quickly';
+
+const wordBlanks =
+  'In the morning i and my ' +
+  myAdjective +
+  ' ' +
+  myNoun +
+  ' ' +
+  myVerb +
+  ' from home to stadium ' +
+  myAdverb;
