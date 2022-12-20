1c289fca60bc4febb0b526f14d7630aec7f03e3d
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-20 16:20:22 +0800

    Basic Algorithms Scripting: 5. return largest number in array

---
+++ basic_algorithms_scripting/5_return_largest_number_in_array.js
@@ -0,0 +1,15 @@
+function largestOfFour(arr) {
+  const four = [];
+  for (let i = 0; i < arr.length; i++) {
+    four.push(Math.max(...arr[i]));
+  }
+
+  return four;
+}
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
