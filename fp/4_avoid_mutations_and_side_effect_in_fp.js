f442235d7038ef85facfba7e52814901b2f0984b
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-22 16:19:06 +0800

    OOP: 4. avoid mutations and side effect in fp

---
+++ fp/4_avoid_mutations_and_side_effect_in_fp.js
@@ -0,0 +1,12 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+  const newValue = fixedValue + 1;
+  return newValue;
+
+  // Only change code above this line
+}
+
+console.log(incrementer());
